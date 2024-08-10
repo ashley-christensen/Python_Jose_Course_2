1f6a3c6800e1b612cce828b385890ea772809bc7
Ashley <reactandfloat@gmail.com>
2024-08-10 08:36:11 -0700

    update

--- datastructures.py
+++ datastructures.py
@@ -1,2 +1,20 @@
 name = "sam"
-print(name[0])
+S = name[0]
+# # print(name[0])
+# # print(name)
+# last_letters = name[1:]
+# # print(name + last_letters + (last_letters*3))
+# # last_letters * 3
+# # S*3
+# 's'*3
+
+x = "Hello World"
+x.upper()
+
+
+print(x.upper())
+print(x.lower())
+print(x.split())
+
+x = 'hi this is a string'
+print(x.split())

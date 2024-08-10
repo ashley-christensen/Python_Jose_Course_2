b69de68c3963c0fbd277830066e09f58e21f601e
Ashley <reactandfloat@gmail.com>
2024-08-10 08:47:38 -0700

    comment

--- datastructures.py
+++ datastructures.py
@@ -8,13 +8,17 @@ S = name[0]
 # # S*3
 # 's'*3
 
-x = "Hello World"
-x.upper()
+# x = "Hello World"
+# x.upper()
 
 
-print(x.upper())
-print(x.lower())
-print(x.split())
+# print(x.upper())
+# print(x.lower())
+# print(x.split())
 
-x = 'hi this is a string'
-print(x.split())
+# x = 'hi this is a string'
+# print(x.split())
+
+# print('This is a string {}'.format('WHICH IS INSIDE MY CODE NOW'))
+# print('The {2} {1} {0}'. format('ashley', 'baby', 'Will'))
+print('The {q} {b} {f}'.format(f='fox', b="brown", q='quick'))

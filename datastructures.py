8de598a23e371c6c95fc548bd456c3147dbffdb0
Ashley <reactandfloat@gmail.com>
2024-08-10 08:55:17 -0700

    update

--- datastructures.py
+++ datastructures.py
@@ -1,5 +1,5 @@
-name = "sam"
-S = name[0]
+# name = "sam"
+# S = name[0]
 # # print(name[0])
 # # print(name)
 # last_letters = name[1:]
@@ -20,5 +20,14 @@ S = name[0]
 # print(x.split())
 
 # print('This is a string {}'.format('WHICH IS INSIDE MY CODE NOW'))
-# print('The {2} {1} {0}'. format('ashley', 'baby', 'Will'))
-print('The {q} {b} {f}'.format(f='fox', b="brown", q='quick'))
+# # print('The {2} {1} {0}'. format('ashley', 'baby', 'Will'))
+# print('The {q} {b} {f}'.format(f='fox', b="brown", q='quick'))
+
+# result = 100/777
+# print(result)
+# print('the result was.. {r:1.2}'.format(r = result))
+
+name = 'Ashley'
+age = 35
+
+print(f'Hello, her name is {name} and she is {age} years old ')

0855ee034214e742e2e31366bf658cdb7216d386
Ashley <reactandfloat@gmail.com>
2024-08-19 13:59:27 -0700

    for loops

--- pythonstatements.py
+++ pythonstatements.py
@@ -1,10 +1,30 @@
 #ctrl + shift + / === run
-if 1 > 3:
-    print('hello')
-elif 2 == 2:
-    print('hello-bye')
-else:
-    print('bye')
+# if 1 > 3:
+#     print('hello')
+# elif 2 == 2:
+#     print('hello-bye')
+# else:
+# #     print('bye')
 
-if 3 >2: 
-    print("two is greater than three ")
+# hungry = True
+# if hungry: 
+#     print("three is greater than two ")
+# else:
+#     print("two is greater than three")
+    
+
+# loc = 'Orlando'
+# if loc == 'Auto Shop':
+#     print("cars are cool!")
+# elif loc == 'Bank':
+#     print("money is cool")
+# elif loc == "Orlando":
+#     print("Florida is so amazing!!")
+# else: 
+#     print("I do not know much")
+
+name = "Ashley"
+fruits = ["apple", "banana", "orange", "mango", "avocado", "coconut", "pineapple"]
+plants = {0: "tree", 2: "flower", 3: "lawn", 4: "houseplant"}
+for item in plants:
+    print(plants[item])

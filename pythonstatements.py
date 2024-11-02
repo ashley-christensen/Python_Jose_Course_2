a144d0cd032f8b8a2e08c87d21971c7578d4bdea
Ashley <reactandfloat@gmail.com>
2024-11-02 15:25:00 -0700

    types of inputs

--- pythonstatements.py
+++ pythonstatements.py
@@ -345,15 +345,21 @@
 
 # result = myfunc()
 
-row1 = ['','','']
-row2 = ['','','']
-row3 = ['','','']
+# row1 = ['','','']
+# row2 = ['','','']
+# row3 = ['','','']
 
 
-def display(row1, row2, row3):
-    print(row1)
-    print(row2)
-    print(row3)
+# def display(row1, row2, row3):
+#     print(row1)
+#     print(row2)
+#     print(row3)
 
-row2[1] = 'X'
-display(row1, row2, row3)
+# row2[1] = 'X'
+# display(row1, row2, row3)
+
+#Input function
+
+result = input("please enter value: ")
+result_int = int(result)
+print (type(result_int))

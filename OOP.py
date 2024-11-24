809ac35df0aa3e76ef0f5cdb1081b0d9e9d9ecc4
Ashley <reactandfloat@gmail.com>
2024-11-24 14:15:32 -0800

    class object attributes

--- OOP.py
+++ OOP.py
@@ -10,11 +10,20 @@
 
 class Dog():
 
-    def __init__(self, breed):
+    #Class Object Attribute
+    #Same for any Instance of Class
+    species = 'mammal'
 
+    def __init__(self, breed, name, spots): 
+
+        #Assign using self.attribute_name
         self.breed = breed
+        self.name = name
 
-my_dog = Dog(breed='Yorkie')
+        #Excpect boolean True/False
+        self.spots = spots
+
+my_dog = Dog(breed='Yorkie', name="Anna", spots=False)
 print(type(my_dog))
 
-print(my_dog.breed)
+print(my_dog.species)

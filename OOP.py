1cdb805c9ce27000db648b2b8235581cfdeaa777
Ashley <reactandfloat@gmail.com>
2025-03-09 09:36:02 -0700

    updated oop

--- OOP.py
+++ OOP.py
@@ -82,4 +82,35 @@ class Dog(Animal):
         Animal.__init__(self)
         print("Dog Created")
 
+    def bark(self):
+        print("WOOF")
+
+    def eat(self):
+        print("I am a dog and I am eating")
+        
+
+
 mydog = Dog()
+mydog.eat()
+
+class Book():
+
+    def __init__(self, title, author, pages):
+    
+        self.title = title
+        self.author = author
+        self.pages = pages
+    
+    def __str__(self):
+        return f"{self.title} written by {self.author}"
+
+    def __len__(self):
+        return self.pages
+    
+    def __del__(self):
+        print("A book object has been deleted")
+
+b = Book('Python rocks', 'Ashley', 444)
+
+
+print(b)
